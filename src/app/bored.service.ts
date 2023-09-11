0c71711d96ccac3e6c0f957a8f429feb091e1b2b
pavan-frontend <144331900+pavan-frontend@users.noreply.github.com>
2023-09-11 11:05:08 +0530

    serives injected

--- src/app/bored.service.ts
+++ src/app/bored.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class BoredService {
 
   constructor(private _httpClient: HttpClient) { }
-  getbored(): Observable<any> {
+  getbored():Observable<any> {
     return this._httpClient.get("https://www.boredapi.com/api/activity")
   }
 }
