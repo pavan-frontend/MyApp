97cb54031e4c80c4b7fc2c647bf68a03bc445fbb
pavan-frontend <144331900+pavan-frontend@users.noreply.github.com>
2023-09-13 17:50:52 +0530

    component to component completed

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -8,11 +8,33 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class ChildComponent {
   @Input() public ac:number = 0;
   public bc:number = 0;
+ 
 
-  @Output() public bEvent:EventEmitter<any> = new EventEmitter();
+  // @Output() public bEvent:EventEmitter<any> = new EventEmitter();
 
-  send(){
-    this.bEvent.emit(this.bc);
+  // send(){
+  //   this.bEvent.emit(this.bc);
+  // }
+
+  // @Input() public rate:number = 0;
+
+  @Input() public n1:number = 0;
+  @Input() public n2:number = 0;
+
+  @Output() cal:EventEmitter<any>=new EventEmitter();
+
+  sum(){
+    this.cal.emit(this.n1+this.n2)
   }
+  sub(){
+    this.cal.emit(this.n1-this.n2)
+  }
+  mul(){
+    this.cal.emit(this.n1*this.n2)
+  }
+  div(){
+    this.cal.emit(this.n1/this.n2)
+  }
+
 
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -7,13 +7,22 @@ import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent {
- public ap:number = 0;
- public bp:number = 0;
+//  public ap:number = 0;
+//  public bp:number = 0;
 
+//  catch(value:any){
+  // this.bp = value;
+//  }
+
+//  public rating:any = 0; 
+
+// calculator
+ public num1:number = 0;
+ public num2:number = 0;
+ public result:number=0;
+ 
  catch(value:any){
-  this.bp = value;
+  this.result = value;
  }
-
- public rating:any = 0; 
 
 }
